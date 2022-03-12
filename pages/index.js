fe258cab34cdfac9720ed0da2dc8ee1108113484
Miguel <shota.merry.go.round@gmail.com>
2022-03-12 19:43:14 +0900

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -18,7 +18,7 @@ export default function Home() {
   }, [])
   async function loadNFTs() {
     /* create a generic provider and query for unsold market items */
-    const infuraId = JSON.parse(process.env.INFURA_ID);
+    const infuraId = JSON.parse(process.env.INFURA_ID.toString().trim() || "");
     const provider = new ethers.providers.JsonRpcProvider(`https://rinkeby.infura.io/v3/${infuraId}`)
     const contract = new ethers.Contract(marketplaceAddress, NFTMarketplace.abi, provider)
     const data = await contract.fetchMarketItems()
