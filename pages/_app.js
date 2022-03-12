7f94dd7a32075e72d5bbca5c03c00450b576aebc
Miguel <shota.merry.go.round@gmail.com>
2022-03-12 22:06:04 +0900

    add responsive menu
    2022/03/12

--- pages/_app.js
+++ pages/_app.js
@@ -5,63 +5,64 @@ import Link from 'next/link'
 function MyApp({ Component, pageProps }) {
   return (
     <div>
-      <div className="max-w-screen-2xl px-4 md:px-8 mx-auto">
+      <div className="max-w-screen-2xl px-4 md:px-8 mx-auto bg-gray-800">
         <header className="flex justify-between items-center py-4 md:py-8">
           <Link href="/">
-            <a className="inline-flex items-center text-black-800 text-2xl md:text-3xl font-bold gap-2.5" aria-label="logo">
+            <a className="inline-flex items-center text-white text-2xl md:text-3xl font-bold gap-2.5" aria-label="logo">
               Miguel’s NFT Store
             </a>
           </Link>
 
           <nav className="hidden lg:flex gap-12">
             <Link href="/">
-              <a className="text-gray-600 hover:text-indigo-500 active:text-indigo-700 text-lg font-semibold transition duration-100">
+              <a className="text-white hover:text-indigo-500 active:text-indigo-700 text-lg font-semibold transition duration-100">
                 買う
               </a>
             </Link>
             <Link href="/create-nft">
-              <a className="text-gray-600 hover:text-indigo-500 active:text-indigo-700 text-lg font-semibold transition duration-100">
+              <a className="text-white hover:text-indigo-500 active:text-indigo-700 text-lg font-semibold transition duration-100">
                 売る
               </a>
             </Link>
             <Link href="/my-nfts">
-              <a className="text-gray-600 hover:text-indigo-500 active:text-indigo-700 text-lg font-semibold transition duration-100">
+              <a className="text-white hover:text-indigo-500 active:text-indigo-700 text-lg font-semibold transition duration-100">
                 My NFTs
               </a>
             </Link>
             <Link href="/dashboard">
-              <a className="text-gray-600 hover:text-indigo-500 active:text-indigo-700 text-lg font-semibold transition duration-100">
+              <a className="text-white hover:text-indigo-500 active:text-indigo-700 text-lg font-semibold transition duration-100">
                 出品中
               </a>
             </Link>
           </nav>
         </header>
       </div>
-      {/* <nav className="border-b p-6">
-        <p className="text-4xl font-bold">NFT Marketplace</p>
-        <div className="flex mt-4">
-          <Link href="/">
-            <a className="mr-4 text-black-500">
-              Buy
-            </a>
-          </Link>
-          <Link href="/create-nft">
-            <a className="mr-6 text-black-500">
-              Sell
-            </a>
-          </Link>
-          <Link href="/my-nfts">
-            <a className="mr-6 text-black-500">
-              My NFTs
-            </a>
-          </Link>
-          <Link href="/dashboard">
-            <a className="mr-6 text-black-500">
-              Dashboard
-            </a>
-          </Link>
-        </div>
-      </nav> */}
+      <div className="visible md:invisible">
+        <nav className="p-6 bg-gray-800">
+          <div className="flex mt-4">
+            <Link href="/">
+              <a className="mr-6 text-white font-semibold">
+                買う
+              </a>
+            </Link>
+            <Link href="/create-nft">
+              <a className="mr-6 text-white font-semibold">
+                売る
+              </a>
+            </Link>
+            <Link href="/my-nfts">
+              <a className="mr-6 text-white font-semibold">
+                My NFTs
+              </a>
+            </Link>
+            <Link href="/dashboard">
+              <a className="mr-6 text-white font-semibold">
+                出品中
+              </a>
+            </Link>
+          </div>
+        </nav>
+      </div>
       <Component {...pageProps} />
     </div>
   )
